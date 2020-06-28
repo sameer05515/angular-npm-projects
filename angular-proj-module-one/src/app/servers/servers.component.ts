a1d21424394900c4c4a0d38ebd3426f2b4b11b40
Premendra Kumar <premendra.kumar@indiabulls.com>
2020-06-28 22:23:27 +0530

    committing at Sun 06/28/2020 22:23:25.60

--- angular-proj-module-one/src/app/servers/servers.component.ts
+++ angular-proj-module-one/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +28,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event:Event){
+    this.serverName = ( <HTMLInputElement> event.target).value;
   }
 
 }
