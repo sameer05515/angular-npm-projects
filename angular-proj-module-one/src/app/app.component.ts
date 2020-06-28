bb0e0c2b239d98ed97e0ceeb0f017848b1e87389
Premendra Kumar <premendra.kumar@indiabulls.com>
2020-06-28 19:52:23 +0530

    committing at Sun 06/28/2020 19:52:22.53

--- angular-proj-module-one/src/app/app.component.ts
+++ angular-proj-module-one/src/app/app.component.ts
@@ -4,6 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles:[`
+  //   h3{
+  //     color : red;
+  //   }
+  // `]
 })
 export class AppComponent {
   //title = 'Premendra Kumar learning angular-proj-module-one';
