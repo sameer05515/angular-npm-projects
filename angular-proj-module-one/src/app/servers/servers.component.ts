f4c141dffbf3951c5a50ac23cddfeb9b1d889d96
Premendra Kumar <premendra.kumar@indiabulls.com>
2020-06-28 20:18:32 +0530

    committing at Sun 06/28/2020 20:18:31.42

--- angular-proj-module-one/src/app/servers/servers.component.ts
+++ angular-proj-module-one/src/app/servers/servers.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template :`
-    <app-server></app-server>
-    <app-server></app-server> `,
-  //templateUrl: './servers.component.html',
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+  // template :`
+  //   <app-server></app-server>
+  //   <app-server></app-server> `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
