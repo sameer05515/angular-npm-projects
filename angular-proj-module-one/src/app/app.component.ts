be8271adb2eca2ec2ea8821b004d0e722d2ffb3b
Sameer.05515 <sameer.geeee@gmail.com>
2020-07-05 12:41:45 +0530

    committing at Sun 07/05/2020 12:41:42.21

--- angular-proj-module-one/src/app/app.component.ts
+++ angular-proj-module-one/src/app/app.component.ts
@@ -20,32 +20,26 @@ export class AppComponent {
   userName = '';
   linkData: Link[] = [];
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
 
     http.get<Link>('assets/data/json/linkData.json')
-    .pipe(map(responseData => {
-      const linkArr: Link[] = [];
-      this.processLinkResponse(responseData, linkArr);
-      return linkArr;
-
-    }
-
-    ))
-    .subscribe( links => {
-     // this.linkData = responseData;
-     console.log('=======>>>>' + links);
-     this.linkData = links;
-
-    }
-
-    );
+      .pipe(map(responseData => {
+        const linkArr: Link[] = [];
+        this.processLinkResponse(responseData, linkArr);
+        return linkArr;
+      }))
+      .subscribe(links => {
+        // this.linkData = responseData;
+        console.log('=======>>>>' + links);
+        this.linkData = links;
+      });
 
   }
 
-  private processLinkResponse(responseData: Link, linkArr: Link[]):void {
+  private processLinkResponse(responseData: Link, linkArr: Link[]): void {
     for (const link in responseData) {
-      console.log('link'+responseData[link]);
-      linkArr.push({...responseData[link]});
+      console.log('link' + responseData[link]);
+      linkArr.push({ ...responseData[link] });
     }
   }
 }
