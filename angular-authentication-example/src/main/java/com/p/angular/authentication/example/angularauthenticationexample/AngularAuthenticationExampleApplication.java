bb41d3ebec785b6abc3824abe2c844b08641e8b2
Sameer.05515 <sameer.geeee@gmail.com>
2020-08-02 16:32:00 +0530

    committing at Sun 08/02/2020 16:31:41.13

---
+++ angular-authentication-example/src/main/java/com/p/angular/authentication/example/angularauthenticationexample/AngularAuthenticationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.p.angular.authentication.example.angularauthenticationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularAuthenticationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularAuthenticationExampleApplication.class, args);
+	}
+
+}
---
+++ angular-authentication-example/src/test/java/com/p/angular/authentication/example/angularauthenticationexample/AngularAuthenticationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p.angular.authentication.example.angularauthenticationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularAuthenticationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
