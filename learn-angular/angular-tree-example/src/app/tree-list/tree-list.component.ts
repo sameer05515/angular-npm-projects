01119563becf82c0a7f7f01ce14fe9d3a5675c66
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-07-09 18:36:32 +0530

    committing at Friday 09 July 2021 06:36:17 PM IST

--- learn-angular/angular-tree-example/src/app/tree-list/tree-list.component.ts
+++ learn-angular/angular-tree-example/src/app/tree-list/tree-list.component.ts
@@ -1,42 +1,71 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector:'app-tree-list',
-    templateUrl:'./tree-list.component.html',
-    styleUrls:['./tree-list.component.css']
+  selector: 'app-tree-list',
+  templateUrl: './tree-list.component.html',
+  styleUrls: ['./tree-list.component.css'],
 })
+export class TreeListComponent implements OnInit {
+  allowNewTreeNode: boolean = false;
+  treeNodeCreationStatus = 'No Node created yet';
+  treeNodeName: string = '';
+  treeNodeParentId: number = 0;
+  treeNodeCreated = false;
+  totalAngularPackages: any;
 
-export class TreeListComponent implements OnInit{
+  constructor(private http: HttpClient) {
+    setTimeout(() => {
+      this.allowNewTreeNode = true;
+    }, 5000);
+  }
 
-    allowNewTreeNode:boolean=false;
-    treeNodeCreationStatus="No Node created yet";
-    treeNodeName:string="";
-    treeNodeCreated=false;
-    totalAngularPackages:any;
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    // this.http.get<any>('http://127.0.0.1:8080/ParentChildRelationsTopics/topics/tree/data').subscribe(data => {
+    //     this.totalAngularPackages = data.data;
+    //     // console.log(data);
+    // })
 
-    constructor(private http: HttpClient){
-        setTimeout(()=>{
-            this.allowNewTreeNode=true;
-        },5000);
-    }
+    // this.http.get<any>('./assets/tree-response.json').subscribe(data => {
+    //     this.totalAngularPackages = data.data;
+    //     // console.log(data);
+    // })
 
-    ngOnInit(){
-        // Simple GET request with response type <any>
-        // this.http.get<any>('http://127.0.0.1:8080/ParentChildRelationsTopics/topics/tree/data').subscribe(data => {
-        //     this.totalAngularPackages = data.data;
-        //     // console.log(data);
-        // })
+    this.getAllNodes();
+  }
 
-        this.http.get<any>('./assets/tree-response.json').subscribe(data => {
-            this.totalAngularPackages = data.data;
-            // console.log(data);
-        })
-    }
+  getAllNodes() {
+    // Simple GET request with response type <any>
+    // this.http.get<any>('http://127.0.0.1:8080/ParentChildRelationsTopics/topics/tree/data').subscribe(data => {
+    //     this.totalAngularPackages = data.data;
+    //     // console.log(data);
+    // })
 
-    onCreateNode(){
-        this.treeNodeCreated=true;
-        this.treeNodeCreationStatus="Node was created and name is "+this.treeNodeName;
-    }
+    this.http
+      .get<any>(
+        'http://127.0.0.1:8080/ParentChildRelationsTopics/topics/tree/data'
+      )
+      .subscribe((data) => {
+        this.totalAngularPackages = data.data;
+        // console.log(data);
+      });
+  }
 
+  onCreateNode() {
+    // this.treeNodeCreated=true;
+    // this.treeNodeCreationStatus="Node was created and name is "+this.treeNodeName;
+
+    this.http
+      .post<any>(
+        'http://127.0.0.1:8080/ParentChildRelationsTopics/topics/tree/data',
+        { title: this.treeNodeName, parentId: this.treeNodeParentId }
+      )
+      .subscribe((data) => {
+        this.treeNodeCreated = true;
+        this.treeNodeCreationStatus =
+          'Node was created and name is ' + this.treeNodeName;
+        this.getAllNodes();
+      });
+  }
 }
