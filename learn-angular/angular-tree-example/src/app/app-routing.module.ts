bf8e8057a9413941e3830e8bb1b882750b3962c5
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-07-31 21:01:34 +0530

    committing at Saturday 31 July 2021 09:01:14 PM IST

--- learn-angular/angular-tree-example/src/app/app-routing.module.ts
+++ learn-angular/angular-tree-example/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TopicListComponent } from './topic-widget/topic-list/topic-list.component';
+
+import { TopicWidgetComponent } from './topic-widget/topic-widget.component';
 import { TreeListComponent } from './tree-list/tree-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tree-list', pathMatch: 'full' },
   { path: 'tree-list', component: TreeListComponent },
-  { path: 'topic-list', component: TopicListComponent }
+  { path: 'topic-list', component: TopicWidgetComponent }
 ];
 
 @NgModule({
