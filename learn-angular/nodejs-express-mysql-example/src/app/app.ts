6b674dd4a41646cd5ed2c26c7aac8db629b9bf29
Premendra Kumar <kumar.premendra@indiabulls.com>
2021-08-29 20:14:10 +0530

    committing at 29-08-2021 20:14:03.98

--- learn-angular/nodejs-express-mysql-example/src/app/app.ts
+++ learn-angular/nodejs-express-mysql-example/src/app/app.ts
@@ -9,7 +9,10 @@ import {wordRouter} from "./routes/words/wordRouter";
 const app = express();
 dotenv.config();
 
-const allowedOrigins = ['http://localhost:3000','http://localhost:4200'];
+const allowedOrigins = ['http://localhost:3000',
+'http://localhost:4200',
+'http://127.0.0.1:3000',
+'http://127.0.0.1:4200'];
 const options: cors.CorsOptions = {
   origin: allowedOrigins
 };
