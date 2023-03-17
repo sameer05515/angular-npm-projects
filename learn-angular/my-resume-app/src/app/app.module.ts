343cb7df7046bb31d27af84833c1ad2499624133
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2023-03-18 05:18:17 +0530

    commit

--- learn-angular/my-resume-app/src/app/app.module.ts
+++ learn-angular/my-resume-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { FactsComponent } from './components/facts/facts.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutComponent,
+    FactsComponent,
+    SkillsComponent,
+    ResumeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
