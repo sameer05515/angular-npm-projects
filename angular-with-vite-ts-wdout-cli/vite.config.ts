2bb8bbd87ffca34c75c05f9f01703501579e1a41
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-28 00:27:41 +0530

    commit

---
+++ angular-with-vite-ts-wdout-cli/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import typescript from '@rollup/plugin-typescript';
+
+export default defineConfig({
+    plugins: [
+        typescript({
+          tsconfig: './tsconfig.json', // Specify your tsconfig.json file
+        }),
+      ],
+    esbuild: {
+        jsxFactory: 'h',
+        jsxFragment: 'Fragment',
+        tsconfigRaw: {
+            compilerOptions: {
+                experimentalDecorators: true,
+                emitDecoratorMetadata: true
+            }
+        }
+    }
+});
