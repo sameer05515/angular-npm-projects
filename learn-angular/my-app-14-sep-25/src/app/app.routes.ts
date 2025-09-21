6d5f0c2cc9bca0d9391e8e777d3b30bd1b9fb5c5
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-09-21 13:59:40 +0530

    commit

--- learn-angular/my-app-14-sep-25/src/app/app.routes.ts
+++ learn-angular/my-app-14-sep-25/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Step1DataRenderer } from './components/step1-data-renderer/step1-data-renderer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"home",
+        pathMatch:"full"
+    },
+    {
+        path:"home",
+        component: Step1DataRenderer
+    }
+];
