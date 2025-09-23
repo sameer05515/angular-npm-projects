9dfcd5503fa38072767f3edd18fefd81c19aa0a3
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-09-23 14:02:38 +0530

    commit

---
+++ learn-angular/my-app-14-sep-25/src/app/components/conv-message-renderer/conv-message-renderer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conv-message-renderer',
+  imports: [],
+  templateUrl: './conv-message-renderer.html',
+  styleUrl: './conv-message-renderer.css'
+})
+export class ConvMessageRenderer {
+
+}
